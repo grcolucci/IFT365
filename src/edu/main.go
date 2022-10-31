2cb54d0c47aaf0d88aba98f5d31a2c7ca4884fd8
grcolucci <Glenn.colucci@gmail.com>
2022-10-31 10:18:06 -0400

    updates

--- src/edu/main.go
+++ src/edu/main.go
@@ -1,3 +1,15 @@
+// ////////////////////////////////////////////////////////////////////////////////////////
+//
+// Filename: main.go
+// File type: Go
+// Author: Glenn Colucci
+// Class: IFT 365
+// Module: 4
+// Date: October 31, 2022
+//
+// Description:
+// Read in 2 files of information for teachers and students
+// Load the info into a holding area and perform functions on the data
 package main
 
 import (
@@ -11,13 +23,27 @@ import (
 
 func main() {
 
+	// Open and read in the teacher file
+	// The contents of the file are read into a slice of type Teacher.
 	teachFile, err := school.ReadTeacherFile("src/edu/teachers.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("\n# of teachers: %d\n", len(teachFile))
+	// Print out the contents of the slice and then formatted labels
 
+	fmt.Println("Teacher slice contents")
+	fmt.Println(teachFile)
+
+	fmt.Printf("\n# of teachers loaded: %d\n", len(teachFile))
+
+	fmt.Println("Teacher Labels:")
+	for _, v := range teachFile {
+		school.PrintAddressLbl(v.Name, v.Address)
+	}
+
+	// Open and read in the student file.
+	// The contents of the file are read in and put into a list of type Student
 	studentFile, err := school.ReadStudentFile("src/edu/students.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -25,9 +51,23 @@ func main() {
 
 	fmt.Printf("\n# of students: %d\n\n", len(studentFile))
 
+	// Print out the contents of the slice and then formated labels
+	fmt.Println("Student slice contents")
+	fmt.Println(studentFile)
+
+	fmt.Println("Student Labels:")
+	for _, v := range studentFile {
+		school.PrintAddressLbl(v.Name, v.Address)
+	}
+
+	// Randomly select one of the teacher records and change the name.
+	// Then print out a new label for that record.
 	rand.Seed(time.Now().UnixNano())
 	randIndex := rand.Intn((len(teachFile) - 1 + 1) + 0)
 	teachFile[randIndex].SetName("Mickey Mouse")
+
+	fmt.Println("Updated teacher label")
+
 	school.PrintAddressLbl(teachFile[randIndex].Name, teachFile[randIndex].Address)
 
 }
--- src/edu/school/school.go
+++ src/edu/school/school.go
@@ -1,3 +1,15 @@
+// ////////////////////////////////////////////////////////////////////////////////////////
+//
+// Filename: school.go
+// File type: Go
+// Author: Glenn Colucci
+// Class: IFT 365
+// Module: 4
+// Date: October 31, 2022
+//
+// Description:
+// Package for processing student and teacher data.
+// storage structs and functions/methods for handling data
 package school
 
 import (
@@ -8,6 +20,7 @@ import (
 	"strconv"
 )
 
+// Comman struct for teacher and student address
 type Address struct {
 	Street  string
 	City    string
@@ -15,12 +28,14 @@ type Address struct {
 	zipCode int
 }
 
+// Student struct
 type Student struct {
 	Studentid int
 	Name      string
 	Address
 }
 
+// Teacher struct
 type Teacher struct {
 	EmployeeID int
 	Name       string
@@ -28,6 +43,9 @@ type Teacher struct {
 	Address
 }
 
+// Mothod to Set and Get the teacher name
+// Set passes in a pointer for the reciever so the name
+// can truely be updated
 func (t *Teacher) SetName(name string) {
 
 	t.Name = name
@@ -38,15 +56,21 @@ func (t Teacher) GetName() string {
 	return t.Name
 }
 
+// Function to print out address labels
+// Input: Sudent/Teacher name field and Address struct/field
 func PrintAddressLbl(inName string, inAddr Address) {
 
 	fmt.Println(inName)
 	fmt.Println(inAddr.Street)
-	fmt.Printf("%s, %s %5d\n\n", inAddr.City, inAddr.State, inAddr.zipCode)
+	fmt.Printf("%s, %s %05d\n\n", inAddr.City, inAddr.State, inAddr.zipCode)
 }
 
+// Read in and store the teacher data
+// Input: file name to be read
+// Output: slice with all the teachers in it.
 func ReadTeacherFile(fName string) ([]Teacher, error) {
 
+	// Open the file
 	csvFile, err := os.Open(fName)
 	if err != nil {
 		return nil, err
@@ -54,17 +78,20 @@ func ReadTeacherFile(fName string) ([]Teacher, error) {
 	fmt.Println("Successfully Opened CSV file")
 	defer csvFile.Close()
 
-	teacherFile := make([]Teacher, 0)
+	teacherFile := make([]Teacher, 0) // Create the teacher slice
 
-	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	csvLines, err := csv.NewReader(csvFile).ReadAll() // Read in all the records
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	// Loop through each line read in and parse the data into
+	// a slice element
 	for i, line := range csvLines {
 
 		var stat Teacher
 
-		intVar, err := strconv.Atoi(line[0])
+		intVar, err := strconv.Atoi(line[0]) // Convert string to int
 		if err != nil {
 			log.Panicf("Error with data record %d skipped", i)
 			continue
@@ -72,6 +99,8 @@ func ReadTeacherFile(fName string) ([]Teacher, error) {
 		stat.EmployeeID = intVar
 
 		stat.Name = line[1]
+
+		// Convert the salary to a float64
 		fltVar, err := strconv.ParseFloat(line[0], 64)
 		if err != nil {
 			log.Panicf("Error with data record %d skipped", i)
@@ -83,6 +112,8 @@ func ReadTeacherFile(fName string) ([]Teacher, error) {
 		stat.Street = line[3]
 		stat.City = line[4]
 		stat.State = line[5]
+
+		// Convert the zip code
 		intVar, err = strconv.Atoi(line[6])
 		if err != nil {
 			log.Panicf("Error with data record %d skipped", i)
@@ -91,19 +122,19 @@ func ReadTeacherFile(fName string) ([]Teacher, error) {
 
 		stat.zipCode = intVar
 
+		// Add new instance to the slice
 		teacherFile = append(teacherFile, stat)
 
 	}
 
-	fmt.Printf("\n# of teachers loaded: %d\n", len(teacherFile))
-
-	for _, v := range teacherFile {
-		PrintAddressLbl(v.Name, v.Address)
-	}
-
+	// Return the slice
 	return teacherFile, nil
 
 }
+
+// Read in and store the student data
+// Input: file name to be read
+// Output: slice with all the students in it.
 
 func ReadStudentFile(fName string) ([]Student, error) {
 
@@ -146,12 +177,6 @@ func ReadStudentFile(fName string) ([]Student, error) {
 
 		studentFile = append(studentFile, stat)
 
-	}
-
-	fmt.Printf("\n# of Students loaded: %d\n", len(studentFile))
-
-	for _, v := range studentFile {
-		PrintAddressLbl(v.Name, v.Address)
 	}
 
 	return studentFile, nil
